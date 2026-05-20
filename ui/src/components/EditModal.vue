ce1ed787b2f969589087216dfbb800efd37490f2
theo.fabien@inrae.fr <theo.fabien@inrae.fr>
2026-05-20 13:49:37 +0200

    Improved EditModal interface

--- ui/src/components/EditModal.vue
+++ ui/src/components/EditModal.vue
@@ -2,11 +2,26 @@
   <div class="modal">
     <div class="modal-content">
       <h3>Edit</h3>
+      <label>Name</label>
       <input v-model="name" placeholder="Name" />
+      <label>Notes</label>
       <textarea rows="6" v-model="notes" placeholder="Notes"></textarea>
       <div v-if="item.type === 'gpx_hikes'">
-        <input v-model="difficulty" placeholder="Difficulty" />
-        <input v-model="gaz" placeholder="Exposure" />
+        <label>Difficulty</label>
+        <select v-model="difficulty">
+          <option value="">Unknown</option>
+          <option value="1">1 - Easy</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5 - Hard</option>
+        </select>
+        <label>Exposure</label>
+        <select v-model="gaz">
+          <option value="">Unknown</option>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
       </div>
       <button class="applyButton" @click="submit">Save</button>
       <button class="applyButton" @click="$emit('close')">Cancel</button>
@@ -65,11 +80,10 @@ async function submit() {
 .modal {
   z-index: 5000;
 }
-.modal-content input,
-.modal-content select,
-.modal-content textarea {
+.modal-content input, select, textarea {
     width: 100%;
     padding: 6px;
     font-size: 14px;
+    margin-bottom: 20px;
 }
 </style>
